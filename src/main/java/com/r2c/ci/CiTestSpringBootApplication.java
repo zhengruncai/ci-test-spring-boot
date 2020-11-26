f704079cb17332b68774c58b7d86decdabb80431
郑润才 <13261763364@163.com>
2020-11-26 13:43:55 +0800

    init

---
+++ src/main/java/com/r2c/ci/CiTestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.r2c.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiTestSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiTestSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/r2c/ci/CiTestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r2c.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTestSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
